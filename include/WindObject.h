fb39c0a0f1ba412899c54e6eb50b53b0c41e88c4
Joshua Weinstein <jweinstein@splunk.com>
2017-11-19 22:42:47 -0800

    started wind obj

---
+++ include/WindObject.h
@@ -0,0 +1,22 @@
+#ifndef WIND_OBJECT_H
+#define WIND_OBJECT_H
+
+
+#define WindObject_BASE struct WindObject next*; \
+        enum WindType type;
+
+enum WindType
+{
+
+};
+
+typedef enum WindType WindType;
+
+struct WindObject
+{
+
+};
+
+typedef struct WindObject WindObject;
+
+#endif
