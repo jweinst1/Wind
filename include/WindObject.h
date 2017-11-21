4b9d0fa3b2014f3f0947038ec2461bb039ca411b
Joshua Weinstein <jweinstein@splunk.com>
2017-11-20 18:38:56 -0800

    widn object definition

--- include/WindObject.h
+++ include/WindObject.h
@@ -1,20 +1,36 @@
 #ifndef WIND_OBJECT_H
 #define WIND_OBJECT_H
 
+#include <stdlib.h>
 
+// base properties to ensure inheritance
 #define WindObject_BASE struct WindObject next*; \
         enum WindType type;
 
+// casts some typed wind object back to its base form.
+#define WindObject_CAST(wobj) ((WindObj*)wobj)
+
+//casts and evaluates to the next of the wind object
+#define WindObject_NEXT(wobj) ((WindObject*)wobj)->next
+
+#define WindObject_IS_END(wobj) wobj->next == NULL
+
+//casts the next of some wind object back to base
+#define WindObject_CAST_NEXT(wobj) (WindObject*)(wobj->next)
+
 enum WindType
 {
-
+        WindType_In,
+        WindType_Out,
+        WindType_Number,
+        WindType_Add
 };
 
 typedef enum WindType WindType;
 
 struct WindObject
 {
-
+        WindObject_BASE
 };
 
 typedef struct WindObject WindObject;
