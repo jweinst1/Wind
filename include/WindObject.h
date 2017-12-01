ab50cc4582f14ef4449c8b25c11d2605833c0fd4
Joshua Weinstein <jweinstein@splunk.com>
2017-12-01 15:49:14 -0800

    remod to windobject, making it pure processor

--- include/WindObject.h
+++ include/WindObject.h
@@ -5,10 +5,23 @@
 #include "WindStr.h"
 #include "WindError.h"
 
+#define WindObject_INS_SIZE 4000
+#define WindObject_CODE_SIZE 1000
+
 // getter macro based on type
 #define WindObject_INT(wobj) wobj->value._int
 
 #define WindObject_STR(wobj) wobj->value._str.begin
+
+enum WindState
+{
+        WindState_Translate,
+        WindState_Transition,
+        WindState_Execution,
+        WindState_Done // for end of current source code reached
+};
+
+typedef enum WindState WindState;
 
 enum WindType
 {
@@ -28,8 +41,12 @@ union WindValue
 
 struct WindObject
 {
+        unsigned char instructions[WindExecutor_INS_SIZE];
         WindError error;
         union WindValue value;
+        unsigned char* insMark;
+        unsigned char* insEnd;
+        WindState state;
         WindType type;
 };
 
