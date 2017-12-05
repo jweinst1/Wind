5c66c889584732031cdc14309e934d30600d0508
Joshua Weinstein <jweinstein@splunk.com>
2017-12-04 23:26:18 -0800

    started and implemented util byte buf class

---
+++ include/ByteBuf.h
@@ -0,0 +1,39 @@
+#ifndef WIND_BYTE_BUF_H
+#define WIND_BYTE_BUF_H
+//util byte buffer type
+
+#include "SafeAlloc.h"
+#include <stdlib.h>
+#include <string.h>
+
+#define ByteBuf_SPACE(bbuf) (bbuf->end - bbuf->mark)
+
+#define ByteBuf_LEN(bbuf) (bbuf->mark - bbuf->begin)
+
+#define ByteBuf_CAP(bbuf) (bbuf->end - bbuf->begin)
+
+struct ByteBuf
+{
+        unsigned char* begin;
+        unsigned char* mark;
+        unsigned char* end;
+};
+
+typedef struct ByteBuf ByteBuf;
+
+//allocates a new byte buffer
+ByteBuf* ByteBuf_new(size_t bufSize);
+
+//writes to, and expands existing byte buffer if neccesary
+void ByteBuf_write(ByteBuf* buf, unsigned char* data, size_t amount);
+
+//frees a byte buffer
+inline void
+ByteBuf_free(ByteBuf* buf)
+{
+        free(buf->begin);
+        free(buf);
+}
+
+
+#endif
---
+++ src/util/ByteBuf.c
@@ -0,0 +1,28 @@
+#include "ByteBuf.h"
+
+ByteBuf* ByteBuf_new(size_t bufSize)
+{
+        ByteBuf* newbuf;
+        SAFE_ALLOC_M(newbuf, sizeof(ByteBuf));
+        SAFE_ALLOC_M(newbuf->begin, (sizeof(unsigned char) * bufSize));
+        newbuf->mark = newbuf->begin;
+        newbuf->end = newbuf->begin + bufSize;
+        return newbuf;
+}
+
+void ByteBuf_write(ByteBuf* buf, unsigned char* data, size_t amount)
+{
+        if(amount > ByteBuf_SPACE(buf))
+        {
+                size_t oldLen = ByteBuf_LEN(buf);
+                size_t newCap = amount + ByteBuf_CAP(buf);
+                SAFE_ALLOC_RE(buf->begin, newCap);
+                buf->mark = buf->begin + oldLen;
+                buf->end = buf->begin + newCap;
+        }
+        else
+        {
+                memcpy(buf->mark, data, amount);
+                buf->mark += amount;
+        }
+}
