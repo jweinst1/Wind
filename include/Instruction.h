d9fdfc74a1c1af0873fc1b44ae2a0ba27d1caef0
Joshua Weinstein <jweinstein@splunk.com>
2017-12-11 22:20:29 -0800

    remove some old code, update to instruction

--- include/Instruction.h
+++ include/Instruction.h
@@ -1,23 +1,18 @@
+
 #ifndef WIND_INSTRUCTION_H
 #define WIND_INSTRUCTION_H
 
-enum WindInstruc
+typedef enum
 {
-        WindInstruc_Nil, // not a true instruction, for zeroing and errors
         WindInstruc_Stop,
-        WindInstruc_In,
-        WindInstruc_Out,
+        WindInstruc_Continue,
+        WindInstruc_Apply,
         WindInstruc_Int,
-        WindInstruc_Str,
-        WindInstruc_List,
+        WindInstruc_ExpStart,
+        WindInstruc_ExpEnd,
+        WindInstruc_ListStart,
         WindInstruc_ListEnd,
-        WindInstruc_Add,
-        WindInstruc_Sub,
-        WindInstruc_Mul,
-        WindInstruc_Div,
-        WindInstruc_Put
-};
-
-typedef enum WindInstruc WindInstruc;
+        WindInstruc_String
+} WindInstruc;
 
 #endif
