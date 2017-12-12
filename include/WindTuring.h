3a2f4a07889ff6b7083e07825d04ad44f9165679
Joshua Weinstein <jweinstein@splunk.com>
2017-12-12 12:11:51 -0800

    turing machine complete

--- include/WindTuring.h
+++ include/WindTuring.h
@@ -6,12 +6,22 @@
 
 // Size of tape for machine.
 // Limits the maximum nested depth that can be computed.
-#define WindTuring_CELL_SIZE 4000
+#define WindTuring_CELL_SIZE 3000
 
+#define WindCell_MAKE(cell, object, inst, state) do { \
+                cell->obj = object; \
+                cell->ins = inst; \
+                cell->active = state; \
+} while(0)
+
+
+// Structure for the machine's cells.
+// @active = indicates whether or not the current
 typedef struct
 {
         WindObject obj;
         WindInstruc ins;
+        int active;
 } WindCell;
 
 WindCell* WindTuring_begin(void);
