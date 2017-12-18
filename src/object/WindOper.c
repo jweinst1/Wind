4aa076c7b51761dbd12731f3a9d6eb9228c6c4ff
Joshua Weinstein <jweinstein@splunk.com>
2017-12-17 21:55:29 -0800

    started on wind oper module

---
+++ src/object/WindOper.c
@@ -0,0 +1,15 @@
+#include "WindOper.h"
+
+void WindOper_add(WindObject* obj1, WindObject* obj2)
+{
+        switch(obj1->type)
+        {
+        case WindInstruc_Int:
+                switch(obj2->type)
+                {
+                default:
+                        return;
+                }
+                return;
+        }
+}
