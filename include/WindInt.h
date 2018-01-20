d916cc8f2597fb5aa48b329c0c724d745cb97adc
Joshua Weinstein <jweinstein@splunk.com>
2018-01-20 14:21:36 -0800

    wind int work

--- include/WindInt.h
+++ include/WindInt.h
@@ -2,8 +2,9 @@
 #define WIND_INT_H
 
 #include "WindObject.h"
+#include "PrintInterface.h"
 
-class WindInt : public WindObject
+class WindInt : public WindObject, public Printable
 {
 public:
 WindInt(long num = 0);
@@ -11,10 +12,22 @@ WindInt(long num = 0);
 {
 }
 
+void debugInfo(void) const
+{
+        std::cout << "Object:{type = Type_Int, value = " << _int << "}\n";
+}
+
+void print(void) const
+{
+        std::cout << _int;
+}
+
 long& getInt(void)
 {
         return _int;
 }
+
+
 private:
 long _int;
 };
