a75515423de51f961d66633a18739a4eb4fc6ccd
Joshua Weinstein <jweinstein@splunk.com>
2018-01-20 23:44:03 -0800

    instruc work start

---
+++ include/Instructions.h
@@ -0,0 +1,14 @@
+#ifndef WIND_INSTRUCTIONS_H
+#define WIND_INSTRUCTIONS_H
+// Instructions related header
+
+struct WindInstruc
+{
+        enum Code
+        {
+                Code_Int,
+                Code_Push
+        }
+};
+
+#endif
