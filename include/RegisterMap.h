3a3b9268e58bd6fe91f88f2b4f4db2c2d056906a
Joshua Weinstein <jweinstein@splunk.com>
2018-01-31 16:35:45 -0800

    register map started

---
+++ include/RegisterMap.h
@@ -0,0 +1,8 @@
+#ifndef REGISTER_MAP_H
+#define REGISTER_MAP_H
+// Header that contains specialized register map structure
+// This structure can facilitate register based instructions with objects.
+
+
+
+#endif
