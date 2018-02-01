d6035010894c07432254a803771fdc294045640d
Joshua Weinstein <jweinstein@splunk.com>
2018-02-01 14:52:28 -0800

    int map work for process compiler

--- include/IntMap.h
+++ include/IntMap.h
@@ -2,8 +2,34 @@
 #define INT_MAP_H
 // Header that contains specialized integer map structure
 // Maps arrays or sequences of chars to an integer, or essentially a slot.
+// Will be used in  process compiler.
 
-#define INT_MAP_NAME_LEN 41
+// Controls the max length of recordable names
+#define INT_MAP_NAME_LEN 51
+// Controls the max size of the internal list
+#define INT_MAP_LST_LEN 10
+
+// Macro that initializes an IntMap
+#define IntMap_INIT(varName) \
+        IntMap varName; \
+        varName.count = 0; \
+        for(int i = 0; i< INT_MAP_LST_LEN; i++) { \
+                varName.items[i].name[0] = '\0'; \
+        }
+
+//represents a node in the map.
+// The position of the node or it's index is the int it maps to.
+typedef struct
+{
+        char name[INT_MAP_NAME_LEN];
+} IntMapNode;
+
+// The actual map structure.
+typedef struct
+{
+        IntMapNode items[INT_MAP_LST_LEN];
+        int count;
+} IntMap;
 
 
 
