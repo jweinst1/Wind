13a17d8cb36bd5044c2a93343e651a54aaae9296
Joshua Weinstein <jweinstein@splunk.com>
2018-02-01 12:15:08 -0800

    work for rpocess

---
+++ include/Instructions.h
@@ -0,0 +1,14 @@
+#ifndef INSTRUCTIONS_H
+#define INSTRUCTIONS_H
+// Header that contains instruction enums for the Wind language
+
+
+// This enum contains instructions for Wind Processess
+// These act as enumarations over byte code
+typedef enum
+{
+        WindPrcIns_Add,
+        WindPrcIns_Sub
+} WindPrcIns;
+
+#endif
---
+++ include/IntMap.h
@@ -0,0 +1,12 @@
+#ifndef INT_MAP_H
+#define INT_MAP_H
+// Header that contains specialized integer map structure
+// Maps arrays or sequences of chars to an integer, or essentially a slot.
+
+#define INT_MAP_NAME_LEN 41
+
+
+
+
+
+#endif
