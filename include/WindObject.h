38c406ab14429f4d8bfc5789cbd2bbe25cf383e5
Joshua Weinstein <jweinstein@splunk.com>
2018-02-07 22:38:05 -0800

    wind object work

--- include/WindObject.h
+++ include/WindObject.h
@@ -12,6 +12,21 @@
 // Simple macro to check if object can fit a size of data.
 #define WindObject_FITS(wobj, size) ((wobj->cap - wobj->len) > size)
 
+#define WindObject_ALLOC(capacity) malloc(sizeof(WindObject) + (capacity * sizeof(unsigned char)))
+
+
+#define WindObject_RESET(wobj) wobj->len = 0
+
+// Evaluates to a pointer which corresponds to the next slot at which data can be written.
+#define WindObject_WRITER(wobj) (wobj->data + wobj->len)
+
+#define WindObject_NULLIFY(wobj) \
+        wobj->next = NULL; \
+        wobj->prev = NULL
+
+#define WindObject_CONNECT(wobj1, wobj2) \
+        wobj1->next = wobj2; \
+        wobj2->prev = wobj1
 
 // Core Object model of wind programming language.
 struct WindObject
