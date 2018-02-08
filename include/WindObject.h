6f9e15b7f01334e29776579f6313401a060df6d5
Joshua Weinstein <jweinstein@splunk.com>
2018-02-08 10:57:37 -0800

    macro for wind object

--- include/WindObject.h
+++ include/WindObject.h
@@ -4,6 +4,9 @@
 
 #include <stdlib.h>
 
+// The amount of extra space to add after expansion
+#define WindObject_ADDSPC 5
+
 #define WindObject_SIZE(wobj) (sizeof(WindObject) + wobj->cap)
 
 #define WindObject_SPACE(wobj) (wobj->cap - wobj->len)
@@ -20,13 +23,29 @@
 // Evaluates to a pointer which corresponds to the next slot at which data can be written.
 #define WindObject_WRITER(wobj) (wobj->data + wobj->len)
 
+
+// Gets a casted pointer corresponding to the address at which new data will be written.
+#define WindObject_WRITER_T(wobj, type) (type*)(wobj->data + wobj->len)
+
 #define WindObject_NULLIFY(wobj) \
         wobj->next = NULL; \
         wobj->prev = NULL
 
+// Expands via realloc the current allocated memory of the object.
+#define WindObject_EXPAND(wobj, amount) do { \
+                wobj = realloc(wobj, wobj->cap + amount + WindObject_ADDSPC); \
+                wobj->cap += amount + WindObject_ADDSPC; \
+} while (0)
+
+// Connects 2 wind objects
 #define WindObject_CONNECT(wobj1, wobj2) \
         wobj1->next = wobj2; \
         wobj2->prev = wobj1
+
+// Disconnects 2 wind objects.
+#define WindObject_DISCON(wobj1, wobj2) \
+        wobj1->next = NULL; \
+        wobj2->prev = NULL
 
 // Core Object model of wind programming language.
 struct WindObject
