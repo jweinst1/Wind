fec8626f2e7853ff65e8a07821927daeac624422
Joshua Weinstein <jweinstein@splunk.com>
2018-05-01 13:58:06 -0700

    changed stream design from * bufs to 2

--- include/WindStream.h
+++ include/WindStream.h
@@ -14,15 +14,22 @@
 #define WindStream_BUF_SIZE 100
 
 // Evaluates to the currently active buffer
-#define WindStream_ACTIVE(ws) (ws->bufs[ws->activeBuf])
+#define WindStream_ACTIVE(ws) (ws->activeBuf)
+
+#define WindStream_ALT(ws) (ws->altBuf)
 // Gets space of active buffer
 #define WindStream_SPACE(ws) (ws->activeBuf->cap - ws->activeBuf->len)
 
 #define WindStream_FITS(ws, size) (ws->activeBuf->cap - ws->activeBuf->len) > size
 
+// Evaluates to a reference to the address to write more data to the buffer.
+#define WindStream_REF(ws) (ws->activeBuf->data + ws->activeBuf->len)
+
+#define WindStream_INC_LEN(ws, amount) (ws->activeBuf->len += amount)
+
 typedef struct
 {
-        WindBuf* bufs[WindStream_BUF_CNT];
+        WindBuf* altBuf;
         WindBuf* activeBuf;
         // Indicates which stream is active, while the other(s) is reserved for copy operations
         int activeIndex;
@@ -34,6 +41,12 @@ WindStream* WindStream_new(void);
 void WindStream_put(WindStream* ws, unsigned char byte);
 
 void WindStream_put_ptr(WindStream* ws, void* ptr, size_t n);
+
+void WindStream_put_int(WindStream* ws, int num);
+
+// Puts string in format [size, data].
+// Does not include typed marker.
+void WindStream_put_string(WindStream* ws, const char* string);
 
 void WindStream_del(WindStream* ws);
 
--- src/stream/WindStream.c
+++ src/stream/WindStream.c
@@ -3,12 +3,9 @@
 WindStream* WindStream_new(void)
 {
         WindStream* newstream = malloc(sizeof(WindStream));
-        for(int i = 0; i < WindStream_BUF_CNT; i++)
-        {
-                newstream->bufs[i] = WindBuf_new(WindStream_BUF_SIZE);
-        }
+        newstream->activeBuf = WindBuf_new(WindStream_BUF_SIZE);
+        newstream->altBuf = WindBuf_new(WindStream_BUF_SIZE);
         newstream->activeIndex = 0;
-        newstream->activeBuf = newstream->bufs[newstream->activeIndex];
         return newstream;
 }
 
@@ -21,12 +18,32 @@ void WindStream_put(WindStream* ws, unsigned char byte)
 void WindStream_put_ptr(WindStream* ws, void* ptr, size_t n)
 {
         if(!(WindStream_FITS(ws, n))) WindBuf_EXPAND(ws->activeBuf, n + 30);
-        memcpy(ws->activeBuf->data, ptr, n);
+        memcpy(ws->activeBuf->data + ws->activeBuf->len, ptr, n);
         ws->activeBuf->len += n;
+}
+
+void WindStream_put_int(WindStream* ws, int num)
+{
+        if(!(WindStream_FITS(ws, sizeof(int)))) WindBuf_EXPAND(ws->activeBuf, sizeof(int) + 30);
+        *(int*)(ws->activeBuf->data + ws->activeBuf->len) = num;
+        ws->activeBuf->len += sizeof(int);
+}
+
+void WindStream_put_string(WindStream* ws, const char* string)
+{
+        unsigned long length = strlen(string);
+        *(unsigned long*)WindStream_REF(ws) = length;
+        ws->activeBuf->len += sizeof(unsigned long);
+
+        if(!(WindStream_FITS(ws, length))) WindBuf_EXPAND(ws->activeBuf, length + 30);
+        char* writer = (char*)WindStream_REF(ws);
+        while(*string) *writer++ = *string++;
+        ws->activeBuf->len += length;
 }
 
 void WindStream_del(WindStream* ws)
 {
-        for(int i = 0; i<WindStream_BUF_CNT; i++) free(ws->bufs[i]);
+        free(ws->activeBuf);
+        free(ws->altBuf);
         free(ws);
 }
